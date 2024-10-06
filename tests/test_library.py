4a7504a29f97ab52e3e636949b033f28686aa8ed
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-10-06 17:47:24 -0400

    Fixing api stuff and making db tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -2,15 +2,18 @@ import unittest
 from library import library
 
 from unittest.mock import Mock
+from unittest.mock import MagicMock
 import json
+
+from library.patron import Patron
 
 class TestLibrary(unittest.TestCase):
 
     def setUp(self):
         self.lib = library.Library()
-        with open('tests_data/books.txt', 'r') as f:
+        with open('tests/library_test_data/books.txt', 'r') as f:
             self.books_data = json.loads(f.read())
-        with open('tests_data/import_json.txt', 'r') as f2:
+        with open('tests/library_test_data/books.txt', 'r') as f2:
             self.json_import = json.loads(f2.read())
 
     ############################################################################
@@ -33,7 +36,7 @@ class TestLibrary(unittest.TestCase):
 
     def test_is_ebook_false(self):
         self.lib.api.get_ebooks = Mock(return_value=self.books_data)
-        self.assertFalse(self.lib.is_ebook('Mein Kampf'))
+        self.assertFalse(self.lib.is_ebook('Fake Book'))
 
 
 
@@ -43,9 +46,9 @@ class TestLibrary(unittest.TestCase):
         self.lib.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertTrue(self.lib.get_ebooks_count('Name of the Wind') > 0)
 
-    def test_get_ebooks_count_fake(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.books_data)
-        self.assertEqual(self.lib.get_ebooks_count('Mein Kampf'), 0)
+    # def test_get_ebooks_count_fake(self):
+    #     self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+    #     self.assertFalse(self.lib.get_ebooks_count('Fake Book'))
 
 
 
@@ -53,39 +56,19 @@ class TestLibrary(unittest.TestCase):
 
     def test_is_book_by_author_real_real(self):
         self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertTrue(self.lib.is_book_by_author('Thoughtful Machine Learning with Python', 'Matthew Kirk'))
+        self.assertTrue(self.lib.is_book_by_author('Matthew Kirk', 'Thoughtful Machine Learning with Python'))
         
     def test_is_book_by_author_real_fake(self):
         self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertFalse(self.lib.is_book_by_author('Thoughtful Machine Learning with Python', 'Matty Jean Lu Picard'))
+        self.assertFalse(self.lib.is_book_by_author('Matty Jean Lu Picard', 'Thoughtful Machine Learning with Python'))
         
     def test_is_book_by_author_fake_real(self):
         self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertFalse(self.lib.is_book_by_author('Thoughtless Machine Learning with Mamba', 'Matthew Kirk'))
+        self.assertFalse(self.lib.is_book_by_author('Matthew Kirk', 'Thoughtless Machine Learning with Mamba'))
         
     def test_is_book_by_author_fake_fake(self):
         self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertFalse(self.lib.is_book_by_author('Thoughtless Machine Learning with Mamba', 'Matty Jean Lu Picard'))
-
-    def test_is_book_by_author_real_real_multiple_of_title(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertTrue(self.lib.is_book_by_author('Python Programming', 'Bill Norton'))
-
-    def test_is_book_by_author_multiple_of_title_other_author(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertTrue(self.lib.is_book_by_author('Python Programming', 'tony f. charles'))
-        
-    def test_is_book_by_author_real_fake_multiple_of_title(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertFalse(self.lib.is_book_by_author('Python Programming', 'Billy Nortaniel'))
-        
-    def test_is_book_by_author_fake_real_multiple_of_title(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertFalse(self.lib.is_book_by_author('Mamba Programming', 'Bill Norton'))
-        
-    def test_is_book_by_author_fake_fake_multiple_of_title(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        self.assertFalse(self.lib.is_book_by_author('Mamba Programming', 'Billy Nortaniel'))
+        self.assertFalse(self.lib.is_book_by_author('Matty Jean Lu Picard', 'Thoughtless Machine Learning with Mamba'))
 
 
 
@@ -93,14 +76,74 @@ class TestLibrary(unittest.TestCase):
 
     def test_get_languages_for_book_real(self):
         self.lib.api.get_ebooks = Mock(return_value=self.json_import)
-        print(self.lib.is_book_by_author('Python Programming'))
-        # I don't think this one works
+        self.assertTrue(self.lib.get_languages_for_book('Python Programming').__contains__("eng"))
 
 
 
     ############################################################################
     ################################# DB METHODS ###############################
     ############################################################################
+
+    # Test register_patron
+
+    def test_register_patron(self):
+        self.lib.register_patron = MagicMock(return_value = 2004)
+        id = self.lib.register_patron("Miles", "Tallia", "20", "2004")
+        self.assertEqual(id, 2004)
+
+
+
+    # Test is_patron_registered
+
+    def test_is_patron_registered(self):
+        self.lib.register_patron("Miles", "Tallia", "20", "2004")
+        patron = Patron("Miles", "Tallia", "20", "2004")
+        self.assertTrue(self.lib.is_patron_registered(patron))
+
+        
+
+    # Test borrow_book
+
+    def test_borrow_book(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+
+        self.lib.register_patron("Miles", "Tallia", "20", "2004")
+        patron = Patron("Miles", "Tallia", "20", "2004")
+
+        self.lib.borrow_book("Name of the Wind", patron)
+
+        self.assertTrue(self.lib.api.get_ebooks)
+
+        
+
+    # Test is_book_borrowed
+
+    def test_is_book_borrowed(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+
+        self.lib.register_patron("Miles", "Tallia", "20", "2004")
+        patron = Patron("Miles", "Tallia", "20", "2004")
+
+        self.lib.borrow_book("Name of the Wind", patron)
+
+        self.assertTrue(self.lib.is_book_borrowed("Name of the Wind", patron))
+
+        
+
+    # Test return_borrowed_book
+
+    def test_return_borrowed_book(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+
+        self.lib.register_patron("Miles", "Tallia", "20", "2004")
+        patron = Patron("Miles", "Tallia", "20", "2004")
+
+        self.lib.borrow_book("Name of the Wind", patron)
+
+        self.lib.return_borrowed_book("Name of the Wind", patron)
+
+        self.assertFalse(self.lib.is_book_borrowed("Name of the Wind", patron))
+        
 
 
 if __name__ == '__main__':
